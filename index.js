8c0480c8c9de32d0c58df8f7694ef7059e4067b3
kannankkking <nagappanramu44@gmail.com>
2024-11-26 18:05:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// JavaScript to toggle the mobile menu with the three dots icon
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
